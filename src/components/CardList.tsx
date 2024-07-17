65d43e063c2b3474571868c75f3cdc57f82962f8
PKfede <47314404+PKfede@users.noreply.github.com>
2024-07-16 23:08:08 -0600

    fixed conflicts

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,24 +2,6 @@ import React from "react";
 import Card from "./Card";
 import { Pagination } from "@mui/material";
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-const CardList = ({ pokemon, setSearchInput }: any) => {
-    return (
-        <>
-            {!pokemon && <p>Not Found</p>}
-            {pokemon && pokemon.map((poke: any, index: any) => {
-                return (
-                    <Card
-                        key={index}
-                        name={poke.name}
-                        setSearchInput={setSearchInput}
-                    />
-                );
-            }
-=======
->>>>>>> Stashed changes
 const CardList = ({
   pokemon,
   found,
@@ -30,10 +12,6 @@ const CardList = ({
   if (!found) {
     return <p>not found2</p>;
   }
-<<<<<<< Updated upstream
-=======
->>>>>>> 86ad2a3cb37cb749441b719f5ab1042e16462d6b
->>>>>>> Stashed changes
 
   return (
     <>
--- src/views/Pokedex.tsx
+++ src/views/Pokedex.tsx
@@ -7,7 +7,6 @@ import styles from "../styles/app.module.css";
 import CardList from "../components/CardList";
 
 const Pokedex = () => {
-<<<<<<< Updated upstream
   const [pokemon, setPokemon] = useState([]);
   const [searchInput, setSearchInput] = useState<string>("");
   const [searchResult, setSearchResult] = useState({});
@@ -16,79 +15,6 @@ const Pokedex = () => {
   const [searching, setSearching] = useState(false);
   const [notFound, setFound] = useState(true);
 
-=======
-<<<<<<< HEAD
-    const [pokemon, setPokemon] = useState<any[]>([]);
-    const [searchInput, setSearchInput] = useState<string>("");
-    const [pageNumber, setPage] = useState<number>(1);
-    const [countNumber, setCount] = useState<number>(0);
-    const [searching, setSearching] = useState(false);
-
-
-    const fetchOne = async () => {
-        try {
-            const apiData = await axios.get(
-                `https://pokeapi.co/api/v2/pokemon/${searchInput}`
-            );
-            setSearching(true);
-            setPokemon([apiData.data]);
-        } catch (error) {
-        }
-    };
-
-    const fetchAll = async () => {
-        const apiData = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon?limit=6&offset=${(pageNumber - 1) * 6
-            }`
-        );
-        setCount(apiData.data.count / 6);
-        setPokemon(apiData.data.results);
-    };
-
-    useEffect(() => {
-
-        if (searchInput !== "") {
-            fetchOne();
-        }
-    }, [searchInput]);
-
-    useEffect(() => {
-        if (searchInput === "") {
-            fetchAll();
-        }
-    }, [searchInput, pageNumber]);
-
-
-    return (
-        <div className={styles.screen}>
-            <Search setSearchInput={setSearchInput} setSearching={setSearching} />
-            <div className={styles.layout}>
-                {searching && <CardStats data={pokemon[0]} />}
-                {!searching && <div className={styles.layout}>
-                    <CardList pokemon={pokemon} count={countNumber} setPage={setPage} setSearchInput={setSearchInput} />
-                    <Pagination
-                        size="large"
-                        count={countNumber}
-                        variant="outlined"
-                        onChange={(e, page) => {
-                            setPage(page);
-                        }}
-                    />
-                </div>
-                }
-            </div>
-        </div>
-    );
-=======
-  const [pokemon, setPokemon] = useState([]);
-  const [searchInput, setSearchInput] = useState<string>("");
-  const [searchResult, setSearchResult] = useState({});
-  const [pageNumber, setPage] = useState<number>(1);
-  const [countNumber, setCount] = useState<number>(0);
-  const [searching, setSearching] = useState(false);
-  const [notFound, setFound] = useState(true);
-
->>>>>>> Stashed changes
   useEffect(() => {
     const fetchApiSearch = async () => {
       try {
@@ -144,10 +70,6 @@ const Pokedex = () => {
       </div>
     </div>
   );
-<<<<<<< Updated upstream
-=======
->>>>>>> 86ad2a3cb37cb749441b719f5ab1042e16462d6b
->>>>>>> Stashed changes
 };
 
 export default Pokedex;
